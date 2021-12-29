33d33664b241dac6b7aa72097622e502a83f5614
Dubey <sundaram.dubey@SW-LP12481.local>
2021-12-29 10:49:25 +0530

    ignore config data

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ class Config(object):
     SECRET_KEY = 'hope-is-a-dangerous-thing-to-lose'
 #    SECRET_KEY = os.environ.get('SECRET_KEY') or 'do-or-do-not-there-is-no-try'
     # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(basedir, 'paasme.db')
-    SQLALCHEMY_DATABASE_URI = "postgresql://sundaramdubey:sundaram@localhost:5432/paasme"
+    SQLALCHEMY_DATABASE_URI = "postgresql://sundaram:sundaram@localhost:5432/paasme"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
